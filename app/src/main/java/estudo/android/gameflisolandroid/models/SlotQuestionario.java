758194c59030d64351e8fa0cfc2bde90b2e078d4
Jordy <jordyfgomes@gmail.com>
2016-03-26 16:32:04 -0300

    ultimo modelo

---
+++ app/src/main/java/estudo/android/gameflisolandroid/models/SlotQuestionario.java
@@ -0,0 +1,11 @@
+package estudo.android.gameflisolandroid.models;
+
+import java.util.Date;
+
+/**
+ * Created by jordy on 26/03/16.
+ */
+public class SlotQuestionario {
+    private Questionario questionario;
+    private Date slotHorario;
+}
