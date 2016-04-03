34f1f9b3983f84594d9873964fd3e795a0fd6944
sandrooliveira1501 <sandrooliveira1501@gmail.com>
2016-04-03 10:12:36 -0300

    Funcionalidade de login finalizada. Criação de progress bar e método para verificação de conta.

--- app/src/main/java/estudo/android/gameflisolandroid/activities/LoginActivity.java
+++ app/src/main/java/estudo/android/gameflisolandroid/activities/LoginActivity.java
@@ -1,16 +1,20 @@
 package estudo.android.gameflisolandroid.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import estudo.android.gameflisolandroid.R;
 import estudo.android.gameflisolandroid.controllers.ControllerLogin;
 import estudo.android.gameflisolandroid.models.Participante;
+import estudo.android.gameflisolandroid.util.PreferencesUtil;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,11 +22,14 @@ public class LoginActivity extends AppCompatActivity {
     private EditText etNumeroInscricao;
     private Button btSignIn;
     private AsyncTaskLogin taskLogin;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         etName = (EditText) findViewById(R.id.name);
 
@@ -36,13 +43,33 @@ public class LoginActivity extends AppCompatActivity {
 
                 String numeroInscricao = etNumeroInscricao.getText().toString().trim();
 
-                //todo validate
-                taskLogin = new AsyncTaskLogin();
-                taskLogin.execute(numeroInscricao);
+                View focusView = null;
+
+                if(etName.getText().toString().isEmpty()){
+                    etName.setError("Campo obrigatório");
+                    focusView = etName;
+                }
+
+                if(etNumeroInscricao.getText().toString().isEmpty()){
+                    etNumeroInscricao.setError("Campo obrigatório");
+                    focusView = etNumeroInscricao;
+                }
+
+                if(focusView == null) {
+                    taskLogin = new AsyncTaskLogin();
+                    taskLogin.execute(numeroInscricao);
+                }else{
+                    focusView.requestFocus();
+                }
 
             }
         });
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        isSigned();
     }
 
     private class AsyncTaskLogin extends AsyncTask<String,Void,Participante>{
@@ -52,7 +79,7 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         protected void onPreExecute() {
             controllerLogin = new ControllerLogin();
-            //todo show progressbar
+            progressBar.setVisibility(View.VISIBLE);
         }
 
         @Override
@@ -68,12 +95,29 @@ public class LoginActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Participante participante) {
-            //todo hide progressbar
-            //todo next activity
             if(participante != null) {
-                Toast.makeText(LoginActivity.this, participante.getEmail(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(LoginActivity.this, participante.getEmail(), Toast.LENGTH_SHORT).show();
+                isSigned();
+            }else{
+                Toast.makeText(LoginActivity.this, "Número de inscrição inexistente!", Toast.LENGTH_SHORT).show();
             }
+
+            progressBar.setVisibility(View.GONE);
+
         }
+
+    }
+
+    public void isSigned(){
+
+        String token = PreferencesUtil.getString(getBaseContext(), Participante.TOKEN);
+
+        if(token == null || token.isEmpty()){
+            return;
+        }
+
+        finish();
+        startActivity(new Intent(getBaseContext(), MenuPrincipal.class));
 
     }
 
